06878b8dc1355ef0a7e93212c99ac80340b1cb76
Aleksandr Pechak <alpechak@gmail.com>
2024-08-08 11:32:42 +0300

    update

---
+++ src/components/App.jsx
@@ -0,0 +1,41 @@
+// export default function App() {
+//   const title ="my react app"
+
+//   const imageTitle = "super beach"
+
+//   const imageURL = "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//   return <>
+//     <h1>{title}</h1>
+//     <img src={imageURL} alt={imageTitle} width="480"/>
+//     <p>
+//       Hello
+//     </p>
+//     </>;
+// }
+
+{
+  "id": "1",
+  "cardNumber": "3456 4567 3784 7688",
+  "cardNumber": "3456 4567 3784 7688",
+  "cardNumber": "3456 4567 3784 7688",
+  "cardNumber": "3456 4567 3784 7688",
+  "cardNumber": "3456 4567 3784 7688",
+  "cardNumber": "3456 4567 3784 7688",
+  "cardNumber": "3456 4567 3784 7688",
+}
+
+export default function App() {
+  return (
+<>
+<div>
+<p>Amount: </p>
+<p>Status: Paid or Pending</p>
+<p>Description: </p>
+<p>Card Number: </p>
+<p>Card Type: </p>
+<p>Card Holder Name: </p>
+<p>Payment Date: </p>
+</div>
+</>
+);
+}
