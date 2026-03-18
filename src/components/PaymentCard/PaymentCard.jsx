a868945c7a64d9582f1f73b5eb21ddd7f00ffd52
Aleksandr Pechak <144905025+AleksandrPechak@users.noreply.github.com>
2026-03-18 18:06:49 +0200

    refactor: restructure components and add styling

--- src/components/PaymentCard/PaymentCard.jsx
+++ src/components/PaymentCard/PaymentCard.jsx
@@ -1,3 +1,6 @@
+import clsx from "clsx";
+import css from "./PaymentCard.module.css";
+
 export default function PaymentCard({
   payment: {
     amount,
@@ -10,17 +13,37 @@ export default function PaymentCard({
   },
 }) {
 
+  // console.log(clsx("a", null && "b", false && "c"));
+
+  // console.log(
+  //   clsx(css.text, {
+  //     [css.isPayd]: isPaid,
+  //     [css.isPanding]: !isPaid,
+  //   })
+  // );
+
+ const amountClassNames = clsx(css.text, isPaid ? css.isPaid : css.isPending);
+
+  // const textClasses = [css.text];
+
+  // if (isPaid) {
+  //   textClasses.push(css.isPaid);
+  // }else {
+  //   textClasses.push(css.isPending);
+  // }
+
+  // console.log(textClasses.join(" "));
+
   return (
     <>
-      <div>
-        <p>Amount: {amount}</p>
-        <p>Status {isPaid ? "Paid" : "Pending"}</p>
-        <p>Description: {description}</p>
-        <p>Card Number: {cardNumber}</p>
-        <p>Card Type: {cardType}</p>
-        <p>Card Holder Name: {cardOwner}</p>
-        <p>Payment Date:{date}</p>
-        <hr />
+      <div className={css.container}>
+        <p className={css.text}>Amount: {amount}</p>
+        <p className={amountClassNames}>Status {isPaid ? "Paid" : "Pending"}</p>
+        <p className={css.text}>Description: {description}</p>
+        <p className={css.text}>Card Number: {cardNumber}</p>
+        <p className={css.text}>Card Type: {cardType}</p>
+        <p className={css.text}>Card Holder Name: {cardOwner}</p>
+        <p className={css.text}>Payment Date:{date}</p>
       </div>
     </>
   );
